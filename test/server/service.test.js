b57f935c47be2d49fca89f18a30c4e79cb71cc71
lynda-danielkhan <lynda@khan.io>
2017-02-15 10:19:15 -0800

    Added logging and fixed style

--- test/server/service.test.js
+++ test/server/service.test.js
@@ -1,8 +1,9 @@
 'use strict';
 
-const should = require('should');
+require('should');
 const request = require('supertest');
-const service = require('../../server/service');
+const config = require('../../config');
+const service = require('../../server/service')(config);
 
 describe('The express service', () => {
     describe('GET /foo', () => {
